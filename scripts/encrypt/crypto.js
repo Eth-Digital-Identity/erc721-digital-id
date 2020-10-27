e98758e00c9f5f42d1c864008709aed1a1f1c414
7-- <regop412@gmail.com>
2020-10-27 00:59:32 -0400

    encrypt values to be put on blockchain

---
+++ scripts/encrypt/crypto.js
@@ -0,0 +1,12 @@
+const cryptoUtils = require('./crypto_utils.js');
+
+//User gives personal data (address, ssn)
+let plaintext = 'my message text';
+//User gives password
+let key = cryptoUtils.getKeyFromPassword("mypassword", cryptoUtils.getSalt());
+//Encrypted value put on blockchain
+let ciphertext = cryptoUtils.encrypt(plaintext, key);
+//Decrypted value read from blockchain
+let decryptOutput = cryptoUtils.decrypt(ciphertext, key);
+
+console.log(decryptOutput.toString('utf8'));
